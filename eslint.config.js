038dbf803527b8dce3cca906b518f807f0463bd7
starlight1509 <nikitapakhomova1234@gmail.com>
2024-12-16 14:35:37 +0700

    meh

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import * as sapphireEslintConfig from '@sapphire/eslint-config';
+import sapphireEslintConfig from '@sapphire/eslint-config';
 import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat();
